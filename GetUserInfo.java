9cfa862fff8af137b84b31d0af294fe0f5c80a6d
nameps <50565475+nameps@users.noreply.github.com>
2020-08-17 17:23:17 +0700

    Add files via upload

---
+++ GetUserInfo.java
@@ -0,0 +1,27 @@
+package week7;
+
+import java.util.Scanner;
+public class GetUserInfo {
+	public static void main(String[]args){
+		String name;
+		int age; 
+		int a,b,c;
+		a=5;
+		b=10;
+		c=20;
+		int total=a*b+c;
+		int total2=a+b*c;
+		int total3=a/b*c;
+		Scanner  inputDevice = new Scanner(System.in);
+		//System.out.println("Please enter your name >>");
+		//name = inputDevice.nextLine();
+		//System.out.println("Please enter your age >>");
+		//age = inputDevice.nextInt();
+		//System.out.print("Your name is"+name);
+		//System.out.println("Your name is"+age);
+		System.out.println("Total a*b+c = "+total);
+		System.out.println("Total a*b+c = "+total2);
+		System.out.println("Total a*b+c = "+total3);
+		
+	}
+}
---
+++ typeCasing.java
@@ -0,0 +1,11 @@
+package week7;
+
+public class typeCasing {
+	public static void main(String[]args) {
+		double PI=3.14159;
+		byte a1 = 3;
+		byte a2 = 5;
+		byte a3 = (byte)(a1+a2);
+		System.out.println(a3);
+	}
+}
